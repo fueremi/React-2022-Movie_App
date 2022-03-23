a027e972245056425c78f5c1e65db8525ba87155
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-23 22:47:29 +0700

    feat(state): adding search value state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import MovieList from "./components/MovieListComponent";
+import NavbarComponent from "./components/NavbarComponent";
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovies = async (payload) => {
+    const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&s=${payload}`;
+    const response = await fetch(API_URL);
+    const result = await response.json();
+
+    if (result.Search) {
+      setMovies(result.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(searchValue);
+  }, [searchValue]);
+
   return (
-    <div className="w-screen h-screen bg-indigo-300 flex justify-center items-center">
-      <h1 className="text-3xl font-bold underline">Movies App</h1>
+    <div className="w-screen h-screen">
+      <div className="container mx-auto px-6">
+        <NavbarComponent
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 };
