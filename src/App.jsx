925a5ac5b0534b53430a8aaba0b9ecf6977857e9
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-23 18:52:30 +0700

    feat(tailwind): adding tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-indigo-300 flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Movies App</h1>
+    </div>
+  );
+};
+
+export default App;
