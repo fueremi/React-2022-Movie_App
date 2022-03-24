1348c40ea76d9fcfce0a2929c13c20208ebf578b
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-24 22:55:09 +0700

    fix(components): fixing movie list showing warning cause key is not unique

--- src/components/MovieListComponent.jsx
+++ src/components/MovieListComponent.jsx
@@ -3,8 +3,8 @@ import MovieCardComponent from "./MovieCardComponent";
 const MovieList = ({ movies }) => {
   return (
     <div className="sm:flex sm:flex-wrap sm:gap-8 justify-center py-8">
-      {movies.map((movie) => (
-        <MovieCardComponent movie={movie} key={movie.imdbID} />
+      {movies.map((movie, index) => (
+        <MovieCardComponent movie={movie} key={index} />
       ))}
     </div>
   );
