6aa18b12e804b4a9abb912ac40b14769bbb841f2
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-24 22:57:25 +0700

    fix(redux): adding searchvalue action and state on navbar component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../redux/actionCreates";
 
 const NavbarComponent = (props) => {
+  const dispatch = useDispatch();
+  const { updateSearchValue } = bindActionCreators(actionCreators, dispatch);
+
+  const handleOnChange = (e) => {
+    updateSearchValue(e.target.value);
+  };
   return (
-    <div className="flex justify-between items-center pt-6">
-      <h1 className="text-3xl font-bold">Movies App</h1>
-      <input
-        className="m-0 shadow appearance-none border rounded w-2/6 xl:w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="search"
-        type="text"
-        value={props.value}
-        onChange={(e) => props.setSearchValue(e.target.value)}
-        placeholder="Search movies ..."
-      ></input>
+    <div className=" py-4 bg-slate-800 w-screen text-slate-200 fixed top-0 left-0 z-30">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Movies App</h1>
+        <input
+          className="m-0 shadow appearance-none border rounded w-2/6 xl:w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="search"
+          type="text"
+          value={props.value}
+          onChange={handleOnChange}
+          placeholder="Search movies ..."
+          onClick={(e) => (e.target.value = "")}
+        ></input>
+      </div>
     </div>
   );
 };
