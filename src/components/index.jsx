a1fc300a5bccf8daa99d6b010ac7193cc9915c4f
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-24 22:56:56 +0700

    fix(components): destructuring compnents to make it more clean

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as LoadingComponent } from "./LoadingComponent";
+export { default as MovieCardComponent } from "./MovieCardComponent";
+export { default as MovieListComponent } from "./MovieListComponent";
+export { default as NavbarComponent } from "./NavbarComponent";
