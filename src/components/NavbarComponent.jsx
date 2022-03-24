c8ec1cfc072d540a47d664d2a8400b394adea38c
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-25 01:02:54 +0700

    fix(style): fixing style based on devices

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../redux/actionCreates";
 
@@ -11,10 +12,12 @@ const NavbarComponent = (props) => {
   };
   return (
     <div className=" py-4 bg-slate-800 w-screen text-slate-200 fixed top-0 left-0 z-30">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Movies App</h1>
+      <div className="max-w-6xl px-6 mx-auto flex justify-center sm:justify-between items-center flex-wrap sm:flex-nowrap gap-y-2 sm:gap-x-4">
+        <h1 className="text-3xl font-bold sm:w-full text-center sm:text-left">
+          <Link to={`/`}>Movies App</Link>
+        </h1>
         <input
-          className="m-0 shadow appearance-none border rounded w-2/6 xl:w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="m-0 shadow appearance-none border rounded w-full lg:w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="search"
           type="text"
           value={props.value}
